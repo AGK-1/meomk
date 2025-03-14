7cdddd0d1db296048c8c56f211f754b838c2ea84
Karimov <121497820+AGK-1@users.noreply.github.com>
2025-03-15 01:58:43 +0400

    Update Program.cs

--- sqli-samko/Program.cs
+++ sqli-samko/Program.cs
@@ -4,30 +4,34 @@ using sqli_samko;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDb>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDb>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Kestrel konfiqurasiyası (80 portu açıq)
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(80); // HTTP üçün
     // options.ListenAnyIP(443, listenOptions => listenOptions.UseHttps()); // HTTPS lazım olarsa
 });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware-lər
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = ""; // Swagger kök URL-də işləsin ("/swagger" yazmadan açmaq üçün)
 });
 
+// app.UseHttpsRedirection(); // HTTPS lazım olsa aktiv edin
 
-//app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
